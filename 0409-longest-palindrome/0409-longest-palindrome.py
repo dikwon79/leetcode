6e99b0cf38007b53d672bf51601c043ce05d5e36
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2024-12-13 09:41:32 +0900

    Time: 1 ms (62.64%), Space: 17.3 MB (11.62%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.py
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -1,24 +1,11 @@
 class Solution:
     def longestPalindrome(self, s: str) -> int:
-        pal = {}
-        count = 0
-        excount = 0
-        for char in s:
-            if char not in pal:
-                pal[char] = 1
-            else:
-                pal[char] += 1
+        count = Counter(s)
+        ans = 0
+        odd = 0
+        for c,val in count.items():
+            ans+=val//2
+            odd = odd or val%2
+        return ans*2 + odd
+
         
-        for key, value in pal.items():
-            
-            if value % 2 == 0:
-                count += value
-            else:
-                count += value-1
-                excount = 1 
-           
-        
-        return count + (1 if excount > 0 else 0)
-                
-            
-            
