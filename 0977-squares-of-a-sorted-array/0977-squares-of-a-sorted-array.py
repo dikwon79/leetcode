aa80610e330c41c565803f4784d649be48480f0f
Dong il Kwon <30903150+dikwon79@users.noreply.github.com>
2025-01-09 16:35:05 +0900

    Time: 0 ms (100%), Space: 19.7 MB (8.82%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -1,21 +1,28 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
+
+
+        # 결과를 저장할 빈 배열
+        n = len(nums)
+        result = [0] * n  # 최종 제곱값을 저장할 배열 (크기 n)
         
-        left, right = 0, len(nums)-1
-        result = [0] * len(nums)
-
-        input_index = len(nums)-1
-        while(left <= right):
-            leftsq = nums[left] ** 2
-            rightsq = nums[right] ** 2
-
-            if leftsq > rightsq:
-                result[input_index] = leftsq
-               
-                left +=1
+        # 투 포인터 초기화
+        left = 0
+        right = n - 1
+        # 결과 배열의 끝에서부터 시작
+        position = n - 1  # 높은 인덱스부터 채워나감
+        
+        while left <= right:
+            left_square = nums[left] ** 2
+            right_square = nums[right] ** 2
+            
+            if left_square > right_square:
+                result[position] = left_square
+                left += 1
             else:
-                result[input_index] = rightsq
-                right -=1
-
-            input_index -=1
+                result[position] = right_square
+                right -= 1
+            
+            position -= 1  # 채우는 위치를 하나씩 줄임
+        
         return result
